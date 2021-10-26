592aaa7b8a574c74a57c74d3d49bc47c764f5325
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2021-10-26 20:29:58 -0300

    Commit das Aulas "Tela de Login: Partes 1, 2 e 3"

---
+++ src/app/pages/account/login/login.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoadingController, NavController, ToastController } from '@ionic/angular';
+import { DataService } from 'src/app/data.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  public hide = true;
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    private navCtrl: NavController,
+    private service: DataService,
+  ) {
+    this.form = this.fb.group({
+      username: ['', Validators.compose([
+        Validators.required,
+      ])],
+      password: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.maxLength(20),
+        Validators.required
+      ])]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  toggleHide() {
+    this.hide = !this.hide;
+  }
+}
