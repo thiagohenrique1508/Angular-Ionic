9684b7abdd23722ab98a136e986d45f242690e25
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2021-10-31 10:33:53 -0300

    Commit da Aula "Estrutura das Páginas"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 // import { AuthorizedGuard } from './guards/authorized.guard';
-// import { FramePage } from './pages/shared/frame/frame.page';
+import { FramePage } from './pages/shared/frame/frame.page';
 
 const routes: Routes = [
   { path: 'login', loadChildren: './pages/account/login/login.module#LoginPageModule' },
   // { path: '', loadChildren: './pages/shared/frame/frame.module#FramePageModule' },
   {
     path: '',
-    // component: FramePage,
+    component: FramePage,
     // canActivate: [AuthorizedGuard],
     children: [
       { path: '', loadChildren: './pages/home/home.module#HomePageModule' },
       { path: 'orders', loadChildren: './pages/store/orders/orders.module#OrdersPageModule' },
       { path: 'orders/:number', loadChildren: './pages/store/order-details/order-details.module#OrderDetailsPageModule' },
     ]
+  },
+  {
+    path: 'frame',
+    loadChildren: () => import('./pages/shared/frame/frame.module').then( m => m.FramePageModule)
+  },
+  {
+    path: 'frameless',
+    loadChildren: () => import('./pages/shared/frameless/frameless.module').then( m => m.FramelessPageModule)
   },
 ];
 
